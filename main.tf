08a9c942f85aee622d96cc7b2045d2aecb15e945
sailatha-sai <82735798+sailatha-sai@users.noreply.github.com>
2021-04-20 00:17:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ variable "vpc_id" {
   default = "vpc-3e34b743"
 }
 variable "infrastructure_version" {
-  default = "1"
+  default = "2"
 }
 terraform {
   backend "s3" {
     encrypt = true
     bucket  = "terraform-bluegreen-testhelloworld"
     region  = "us-east-1"
-    key     = "v1"
+    key     = "v2"
   }
 }
 locals {
@@ -21,11 +21,11 @@ locals {
   availability_zones = ["us-east-1a"]
 }
 
-resource "aws_subnet" "terraform-blue-green" {
+resource "aws_subnet" "terraform-blue-greenv2" {
   count                   = "${local.subnet_count}"
   vpc_id                  = "${var.vpc_id}"
   availability_zone       = "${element(local.availability_zones, count.index)}"
-  cidr_block              = "172.31.${local.subnet_count * (var.infrastructure_version - 1) + count.index + 1}.0/24"
+  cidr_block              = "172.31.${local.subnet_count * (var.infrastructure_version - 0) + count.index + 2}.0/24"
   map_public_ip_on_launch = true
 
   tags = {
@@ -34,15 +34,15 @@ resource "aws_subnet" "terraform-blue-green" {
 }
 
 locals {
-  subnets = "${aws_subnet.terraform-blue-green.*.id}"
+  subnets = "${aws_subnet.terraform-blue-greenv2.*.id}"
   }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "examplev2" {
     count  = 1
     ami = "ami-20ff515a"
     instance_type = "t2.small"
     subnet_id              = "${element(local.subnets, count.index)}"
-    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+    vpc_security_group_ids = ["${aws_security_group.instancev2.id}"]
     key_name               = "terraform-bluegreen"
 # add
     user_data = <<-EOF
@@ -59,13 +59,8 @@ resource "aws_instance" "example" {
     }
 }
 
-resource "aws_lb_target_group_attachment" "terraform-1" {
-    target_group_arn                    = "arn:aws:elasticloadbalancing:us-east-1:376048985616:targetgroup/terraform-bluegreen/0b7aabebcfae1ac9"
-    target_id                           = "${aws_instance.example.0.id}"
-    port                                = 80
-  }
-resource "aws_security_group" "instance" {
-    name = "terraform-example-instancev1"
+resource "aws_security_group" "instancev2" {
+    name = "terraform-example-instancev2"
     ingress {
         from_port = 80
         to_port = 80
@@ -80,3 +75,10 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+
+  resource "aws_lb_target_group_attachment" "terraform-1" {
+    target_group_arn                    = "arn:aws:elasticloadbalancing:us-east-1:376048985616:targetgroup/terraform-bluegreen/0b7aabebcfae1ac9"
+    target_id                           = "${aws_instance.examplev2.0.id}"
+    port                                = 80
+  }
